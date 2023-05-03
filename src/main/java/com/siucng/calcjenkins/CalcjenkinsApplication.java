3966d77712e997ef479c2a6854e77246dbb05e21
siucng2222-c <vagrant@controller.anslab.com>
2023-05-03 08:49:05 +0000

    Add Spring Boot skeleton

---
+++ src/main/java/com/siucng/calcjenkins/CalcjenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.siucng.calcjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcjenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalcjenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/siucng/calcjenkins/CalcjenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siucng.calcjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalcjenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
